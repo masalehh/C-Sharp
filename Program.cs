3ce090c5d7f27c58b68add4b0c4c689cce3dba6f
Md Abdullah Saleh <ma.saleh007@gmail.com>
2021-06-01 05:15:43 +0600

    Simple mad lib game

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace freeCodeCamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluranNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a Pluran Noun: ");
+            pluranNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluranNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
